22c5fbea06ba036c05db1bfa76d6998189b1c7b4
RedMoon32 <rinat.babichev@gmail.com>
2020-04-17 11:47:37 +0400

    labs done (finally)

--- src/win32_window_main.cpp
+++ src/win32_window_main.cpp
@@ -2,7 +2,6 @@
 #include "renderer.h"
 #include "win32_window.h"
 
-
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
 	try
@@ -10,10 +9,12 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nC
 		Renderer render(1280, 720);
 		return Win32Window::Run(&render, hInstance, nCmdShow);
 	}
+
 	catch (com_exception e)
 	{
 		OutputDebugString(L"Exception:\n");
 		OutputDebugString(e.get_wstring());
 		return 1;
 	}
+	
 }
